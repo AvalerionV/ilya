c8871330ad27a2ca2683c53806564115fa350477
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-09-27 17:44:54 +0500

    minor changes

--- source/main.js
+++ source/main.js
@@ -18,7 +18,7 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile(path.join(__dirname, 'index.html'))
     
-  autoUpdater.checkForUpdates()
+  autoUpdater.checkForUpdatesAndNotify()
 
 }
 
