b6324973df21fd561ea26166cd32ba8c0d662db9
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-09-28 10:44:54 +0500

    auth()

---
+++ source/assets/js/auth.js
@@ -0,0 +1,12 @@
+function auth() {
+    
+    var email = $(".l-user").val();
+    var password = $(".p-user").val();
+    
+    // Register a new user
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function (err) {
+        // Handle errors
+        console.log(err);
+    });
+}
