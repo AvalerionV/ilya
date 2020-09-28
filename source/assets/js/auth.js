534cd16185aabc835244bca243beebaf88ae5df1
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-09-28 11:17:04 +0500

    minor changes

--- source/assets/js/auth.js
+++ source/assets/js/auth.js
@@ -1,4 +1,25 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        
+        afterLogin();
+        // ...
+    } else {
+        // User is signed out.
+        // ...
+    }
+});
+
 function auth() {
+    
+    hideElements();
     
     var email = $("#l-user").val();
     var password = $("#p-user").val();
@@ -8,7 +29,21 @@ function auth() {
         var errorCode = error.code;
         var errorMessage = error.message;
         
-        console.log(errorCode);
+        register(email, password);
+
+        // ...
+    });
+}
+
+function register(email, password) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        $(".error").slideDown();
+        $("#e-msg").html(errorMessage);
+
         // ...
     });
 }
---
+++ source/assets/js/show.js
@@ -0,0 +1,8 @@
+function afterLogin() {
+    $(".login-b").hide();
+    
+    $(".navbar").show(); 
+    $(".topbar").show();
+    $(".footer").show(); 
+    $(".dashboard-b").show();
+}
