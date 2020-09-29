063b9a1c39a983c3c9e96a238bcb98c053cb282c
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-09-29 16:07:40 +0500

    filter minor changes

--- source/main.js
+++ source/main.js
@@ -5,8 +5,6 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 1000,
-    height: 700,
     title: "Blue Caribbean Properties - Listing Management System",
     webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
