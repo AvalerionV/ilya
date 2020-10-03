a693b38565cd2ceae7a6e19e31206d3ae162f060
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-10-03 13:43:31 +0500

    fetch listing

---
+++ source/assets/js/fetchData.js
@@ -0,0 +1,75 @@
+//let options = {where: [["location", "==", "Cocotal Golf Course - Bavaro"]]};
+let options = {};
+
+function readDocuments(collection, options = {}) {
+    let {where, orderBy, limit} = options;
+    let query = firebase.firestore().collection(collection);
+
+    if (where) {
+        if (where[0] instanceof Array) {
+            // It's an array of array
+            for (let w of where) {
+                query = query.where(...w);
+            }
+        } else {
+            query = query.where(...where);
+        }
+
+    }
+
+    if (orderBy) {
+        query = query.orderBy(...orderBy);
+    }
+
+    if (limit) {
+        query = query.limit(limit);
+    }
+
+    return query
+            .get()
+            .then()
+            .catch()
+}
+
+function getFormattedDate() {
+    var date = new Date();
+
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    month = (month < 10 ? '0' : '') + month;
+    day = (day < 10 ? '0' : '') + day;
+    hour = (hour < 10 ? '0' : '') + hour;
+    min = (min < 10 ? '0' : '') + min;
+    sec = (sec < 10 ? '0' : '') + sec;
+
+    var str = day + '/' + month + '/' + date.getFullYear();
+
+    return str;
+}
+
+function renderList(doc) {
+    
+    $('.listing').append('<tr class="item-data"><td><label class="container"><input type="checkbox"></label></td><td>' + getFormattedDate(doc.data().date) + '</td><td>' + doc.data().name + '</td><td>' + doc.data().location + '</td><td>'+ doc.data().typeofproperty +'</td><td>' + doc.data().status + '</td></tr>')
+
+}
+
+fetchListing('listing');
+
+function fetchListing(collection) {
+    
+    $(".listing tr").detach(); // clear the table data
+    
+    let documents = readDocuments(collection, options); // read collection from firestore using provided options argument
+    
+    documents.then(function(doc) {
+        doc.docs.forEach(doc => {
+            renderList(doc)
+        })
+    })
+    
+}
+
