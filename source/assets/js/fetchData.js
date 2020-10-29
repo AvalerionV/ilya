95b63a1ee8f18d79c565e521a85d03e67e133fb0
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-10-29 19:59:16 +0500

    minor design changes

--- source/assets/js/fetchData.js
+++ source/assets/js/fetchData.js
@@ -81,7 +81,7 @@ function returnDate(dt) {
 
 function renderList(id,doc) {
     
-    $('.listing .listing-table-data').append('<tr class="item-data" onclick="openDocument(\'' + id + '\')"><td>' + returnDate(doc.date) + '</td><td>' + doc.name + '</td><td>' + doc.location + '</td><td>'+ doc.top +'</td><td>' + getAgent(doc.agent) + '</td>' + getListingStatus(doc.status) + '</tr>');
+    $('.listing .listing-table-data').append('<tr class="item-data" onclick="openDocument(\'' + id + '\')"><td><div id="l-name">' + doc.name + '</div><div id="l-location">' + doc.location + '</div></td><td>' + returnDate(doc.date) + '</td><td>'+ doc.top +'</td><td>' + getAgent(doc.agent) + '</td>' + getListingStatus(doc.status) + '</tr>');
     
     if (checkLocationExists(doc.location) == false) {
         locationArray.push(doc.location);
@@ -91,7 +91,7 @@ function renderList(id,doc) {
 
 function renderAgent(id,doc) {
     
-    $('.agent .agent-table-data').append('<tr class="item-data"><td>' + doc.name + '</td><td>' + doc.location + '</td>' + getAgentStatus(doc.status) + '</tr>')
+    $('.agent .agent-table-data').append('<tr class="item-data"><td><div id="a-name">' + doc.name + '</div><div id="a-location">' + doc.location + '</div></td>' + getAgentStatus(doc.status) + '</tr>')
     
     if (!checkAgentExists(doc.name)) {
         agentNameArray.push(doc.name);
