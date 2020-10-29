6515fb1136a04340dd1b816fb386f1555215fe4c
Dawood Khan Masood <62904590+AvalerionV@users.noreply.github.com>
2020-10-29 13:18:53 +0500

    Update main.js

--- source/main.js
+++ source/main.js
@@ -1,18 +1,7 @@
-const { app, BrowserWindow, webContents, ipcMain } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const { autoUpdater } = require("electron-updater")
 const log = require('electron-log')
 const path = require('path')
-
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
-
-let win;
-
-async function sendStatusToWindow (text) {
-  log.info(text);
-  win.webContents.send('message', text);
-}
 
 function createWindow () {
     
@@ -25,54 +14,15 @@ function createWindow () {
             contextIsolation: true
         }
     })
-    
-    win.webContents.openDevTools()
 
     win.setMenuBarVisibility(false)
 
     // and load the index.html of the app.
     win.loadFile(path.join(__dirname, 'index.html'))
 
-    autoUpdater.checkForUpdatesAndNotify().then(function() {
-        sendStatusToWindow('Checking for update...');
-    })
+    autoUpdater.checkForUpdatesAndNotify();
     
 }
-
-/*checking for updates*/
-autoUpdater.on("checking-for-update", () => {
-    sendStatusToWindow('Checking for update...');
-});
-
-/*No updates available*/
-autoUpdater.on("update-not-available", info => {
-    sendStatusToWindow('Update not available.');
-});
-
-/*New Update Available*/
-autoUpdater.on("update-available", info => {
-    sendStatusToWindow('Update available.');
-});
-
-/*Download Status Report*/
-autoUpdater.on("download-progress", progressObj => {
-    sendStatusToWindow('Download progress...');
-});
-
-/*Download Completion Message*/
-autoUpdater.on("update-downloaded", info => {
-    sendStatusToWindow('Update downloaded; will install in 5 seconds');
-});
-
-/*Install And Quit*/
-autoUpdater.on('update-downloaded', (ev, info) => {
-  // Wait 5 seconds, then quit and install
-  // In your application, you don't need to wait 5 seconds.
-  // You could call autoUpdater.quitAndInstall(); immediately
-  setTimeout(function() {
-    autoUpdater.quitAndInstall();  
-  }, 5000)
-})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
